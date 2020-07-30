3edb14c6bf0c08dda30d3e637eebafe03dbb63ac
Nirmani <deeshaniathuraliya2@gmail.com>
2020-07-30 17:28:34 +0530

    Second Commit

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.usingstrings;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.TaskStackBuilder;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.widget.TextView;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView3;
-
+    private static final String TAG = "MainActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
         textView3 = findViewById(R.id.textMsg2);
         textView3.setText(R.string.Msg2);
-        
+        Log.i(TAG, "onCreate: ");
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
 }
